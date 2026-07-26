62cacc352317e0c278841d6f5ae36018e73cbc4b
Eddie <ed.murillo717@gmail.com>
2026-07-26 15:52:17 -0500

    chore: renaming files

---
+++ lib/arg_parse.py
@@ -0,0 +1,43 @@
+import argparse
+
+class ArgParser(argparse.ArgumentParser):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add_args()
+
+    def add_args(self):
+
+        # Album/Playlist
+        self.add_argument(
+                        "-f",
+                        "-folder",
+                        dest="folder",
+                        help="Specify Album/Playlist to test")
+        # Song
+        self.add_argument(
+                        "-s",
+                        "-song",
+                        dest="song",
+                        help="Specify single song to test")
+        
+        #spotify link
+        self.add_argument(
+                        "-sp",
+                        "--spotify",
+                        dest="spotify_link",
+                        help="Specify Spotify Link (playlist/song) to analyze")
+
+        #youtube link
+        self.add_argument(
+                        "-yt",
+                        "--youtube",
+                        dest="youtube_link",
+                        help="specify Youtube Link ((playlist/song) to analyze)")
+        
+        self.add_argument(
+                        "-k",
+                        "--keep",
+                        dest="keep",
+                        default=False,
+                        action="store_true",
+                        help="Keep any downloaded music after analysis")
---
+++ lib/audio_fetcher.py
@@ -0,0 +1,71 @@
+from spotdl import Spotdl
+import yt_dlp
+import os
+from lib.song import Song
+import lib.output as out
+
+def _construct_path(keep):
+    if keep:
+        return "./music/"
+    else:
+        return "./temp/"
+
+def get_spotify_song(url, keep):
+
+    out.spotify()
+    dir = _construct_path(keep)
+
+    spotdl_client = Spotdl(
+        client_id="",
+        client_secret="",
+        downloader_settings={
+            "output":"{0}{{title}}.{{output-ext}}".format(dir+"{album}/"),
+            "format":"mp3",
+        }
+    )
+
+    songs = spotdl_client.search([url])
+    downloaded_files = spotdl_client.download_songs(songs)
+
+    songs_list = []
+    for song, path in downloaded_files:
+        if path is None or not os.path.exists(str(path)):
+            print("[!] Skipping {0}: Download failed. Try this song again later".format(song.name))
+        songs_list += (Song(path))
+
+    out.spotify_complete()
+    return songs_list
+
+def get_youtube_song(url, keep):
+
+    out.youtube()
+    dir = _construct_path(keep)
+    ydl_opts = {
+
+        "format":"bestaudio/best",
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3', 
+            'preferredquality': '192',
+        }],
+        'paths': {"home": dir},
+        'outtmpl': "%(title)s.%(ext)s", 
+        'quiet': True,
+    }
+
+    song_list = []
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        filename = info.get('requested_downloads', [{}])[0].get('filepath')
+        
+        if not filename:
+            filename = ydl.prepare_filename(info)
+            if not filename.endswith('.mp3'):
+                filename = filename.rsplit('.', 1)[0] + '.mp3'
+
+        song_list += (Song(filename))
+    out.youtube_complete()
+    return song_list
+
+        
